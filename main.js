866f16661ef133ab1519c11ac76dee6a2d1f5461
Bola Muyis <bolamuyis@gmail.com>
2018-11-30 20:30:01 -0500

    updates with images and js

--- main.js
+++ main.js
@@ -2,3 +2,11 @@
 
 //Javascript connection established, YES!
 //alert("Hello");
+
+let image = document.getElementsByTagName('img');
+
+let img1 = image[0]
+window.addEventListener('click', (event)=>{
+    img1.classList.toggle('spin')
+});
+
